31eb6a4b9c4ce05da2dec4780d769faed10463c5
blazhkevych.v <blazhkevych.v@gmail.com>
2022-11-10 14:06:31 +0200

    правки

--- task/Form1.Designer.cs
+++ task/Form1.Designer.cs
@@ -158,7 +158,9 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Игра «Крестики-нолики».";
             this.ResumeLayout(false);
             this.PerformLayout();
