3f3f3f73edd24604eca29c3fea49ffa8000baad0
blazhkevych.v <blazhkevych.v@gmail.com>
2022-11-10 14:14:43 +0200

    buttons_Click

--- task/Form1.Designer.cs
+++ task/Form1.Designer.cs
@@ -60,6 +60,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttons_Click);
             // 
             // button3
             // 
@@ -70,6 +71,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.buttons_Click);
             // 
             // button4
             // 
@@ -80,6 +82,7 @@
             this.button4.TabIndex = 5;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.buttons_Click);
             // 
             // button5
             // 
@@ -90,6 +93,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.buttons_Click);
             // 
             // button6
             // 
@@ -100,6 +104,7 @@
             this.button6.TabIndex = 3;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.buttons_Click);
             // 
             // button7
             // 
@@ -110,6 +115,7 @@
             this.button7.TabIndex = 8;
             this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.buttons_Click);
             // 
             // button8
             // 
@@ -120,6 +126,7 @@
             this.button8.TabIndex = 7;
             this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.buttons_Click);
             // 
             // button9
             // 
@@ -130,6 +137,7 @@
             this.button9.TabIndex = 6;
             this.button9.Text = "button9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.buttons_Click);
             // 
             // RulesOfTheGame
             // 
